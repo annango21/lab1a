aee42cfcf1e0deb44e02b93dd73288c45459b23c
NSCC Student <NSCCStudent@localhost.localdomain>
2020-02-15 23:14:34 -0400

    done but cannot add a migration for the rating field

--- lab1a/Models/SeedData.cs
+++ lab1a/Models/SeedData.cs
@@ -1,3 +1,6 @@
+﻿﻿#define Rating
+#if Rating
+#region snippet_1 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using lab1a.Data;
@@ -11,8 +14,7 @@ namespace lab1a.Models
         public static void Initialize(IServiceProvider serviceProvider)
         {
             using (var context = new RazorPagesMovieContext(
-                serviceProvider.GetRequiredService<
-                    DbContextOptions<RazorPagesMovieContext>>()))
+                serviceProvider.GetRequiredService<DbContextOptions<RazorPagesMovieContext>>()))
             {
                 // Look for any movies.
                 if (context.Movie.Any())
@@ -20,21 +22,25 @@ namespace lab1a.Models
                     return;   // DB has been seeded
                 }
 
+                #region snippet1
                 context.Movie.AddRange(
                     new Movie
                     {
                         Title = "When Harry Met Sally",
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Romantic Comedy",
-                        Price = 7.99M
+                        Price = 7.99M,
+                        Rating = "R"
                     },
+                #endregion
 
                     new Movie
                     {
                         Title = "Ghostbusters ",
                         ReleaseDate = DateTime.Parse("1984-3-13"),
                         Genre = "Comedy",
-                        Price = 8.99M
+                        Price = 8.99M,
+                        Rating = "G"
                     },
 
                     new Movie
@@ -42,7 +48,8 @@ namespace lab1a.Models
                         Title = "Ghostbusters 2",
                         ReleaseDate = DateTime.Parse("1986-2-23"),
                         Genre = "Comedy",
-                        Price = 9.99M
+                        Price = 9.99M,
+                        Rating = "G"
                     },
 
                     new Movie
@@ -50,7 +57,8 @@ namespace lab1a.Models
                         Title = "Rio Bravo",
                         ReleaseDate = DateTime.Parse("1959-4-15"),
                         Genre = "Western",
-                        Price = 3.99M
+                        Price = 3.99M,
+                        Rating = "NA"
                     }
                 );
                 context.SaveChanges();
@@ -58,3 +66,5 @@ namespace lab1a.Models
         }
     }
 }
+#endregion
+#endif
